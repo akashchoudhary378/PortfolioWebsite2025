2fbe040c3e384a625b559c6018d9a435bdcfa08d
akashchoudhary378 <choudharyakash378@gmail.com>
2025-05-02 12:46:31 +0530

    FooterUpdate

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center w-full p-5 bg-zinc-800">
+    <div className="flex text-sm lg:text-md tracking-tighter items-center justify-center w-full p-5 bg-zinc-800">
       <span className='font-medium text-center tracking-tighter'>Copyright &#169; 2025 Akash Choudhary. All Right Reserved</span>
     </div>
   );
